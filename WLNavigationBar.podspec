2ba5c6e1cf50408f7f37b1f8f09e92089205fd6b
liwei02 <weil218@163.com>
2019-07-04 10:16:19 +0800

    新增podspec

---
+++ WLNavigationBar.podspec
@@ -0,0 +1,16 @@
+
+Pod::Spec.new do |spec|
+  spec.name         = "WLNavigationBar"
+  spec.version      = "0.0.2"
+  spec.summary      = "自定义导航栏"
+  spec.description  = "自定义的导航栏组件，支持左右以及标题item的增加删除及更新，支持所有item的自定义及事件，支持所有item的透明度变化和颜色变化，支持导航栏背景色的变化等"
+  spec.homepage     = "https://github.com/weilLove/WLCustomNavigationBar"
+  spec.license      = { :type => "MIT", :file => "LICENSE" }
+  spec.author             = { "weilLove" => "weil218@163.com" }
+  spec.platform     = :ios, "8.0"
+  spec.source       = { :git => "https://github.com/weilLove/WLCustomNavigationBar.git", :tag => "#{spec.version}" }
+  spec.frameworks = "UIKit"
+  spec.subspec 'Core'  do |core|
+        core.source_files = "WLNavigationBar/Core/**/*.{h,m}"
+  end
+end
---
+++ WLNavigationBar/Core/WLNavigationBar.h
@@ -0,0 +1,66 @@
+//
+//  WLNavigationBar.h
+//  WLCustomNavigationBar
+//
+//  Created by weil on 2019/7/3.
+//  Copyright © 2019 AllYoga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WLNavigationBar : UIView
+//左边新增item
+- (void) wl_addLeftItem:(UIView *)item;
+- (void) wl_insertLeftItem:(UIView *)item atIndex:(int)index;
+//右边新增或者插入item
+- (void) wl_addRightItem:(UIView *)item;
+- (void) wl_insertRightItem:(UIView *)item atIndex:(int)index;
+//设置标题item
+- (void) wl_setTitleItem:(UIView *)item;
+//左边删除item
+- (void) wl_removeLeftItem:(UIView *)item;
+- (void) wl_removeLeftItemAtIndex:(int)index;
+//右边删除item
+- (void) wl_removeRightItem:(UIView *)item;
+- (void) wl_removeRightItemAtIndex:(int)index;
+//更新左侧item
+- (void) wl_updateLeftItem:(UIView *)item atIndex:(int)index;
+//更新所有左侧item的透明度
+- (void) wl_updateLeftAlpha:(CGFloat)alpha;
+//更新左侧某一个item的透明度
+- (void) wl_updateLeftItemAlpha:(CGFloat)alpha atIndex:(int)index;
+//更新左侧所有item的背景色
+- (void) wl_updateLeftBackgroundColor:(UIColor *)color;
+//更新左右某一个item的背景色
+- (void) wl_updateLeftItemBackgroundColor:(UIColor *)color atIndex:(int)index;
+//更新右侧item
+- (void) wl_updateRightItem:(UIView *)item atIndex:(int)index;
+//更新所有右侧item的透明度
+- (void) wl_updateRightAlpha:(CGFloat)alpha;
+//更新右侧某一个item的透明度
+- (void) wl_updateRighItemAlpha:(CGFloat)alpha atIndex:(int)index;
+//更新右侧所有item的背景色
+- (void) wl_updateRightBackgroundColor:(UIColor *)color;
+//更新右侧某一个item的背景色
+- (void) wl_updateRightItemBackgroundColor:(UIColor *)color atIndex:(int)index;
+//更新标题item
+- (void) wl_updateTitleItem:(UIView *)item;
+//更新标题的透明度
+- (void) wl_updateTitleAlpha:(CGFloat)alpha;
+//更新标题的颜色
+- (void) wl_updateTitleColor:(UIColor *)color;
+//设置背景色,可以通过颜色的colorWithAlphaComponent设置透明度
+- (void) wl_setBackgroundColor:(UIColor *)backgroundColor;
+//设置透明度，背景颜色采用上述设置的
+- (void) wl_setAlpha:(CGFloat)alpha;
+//布局子视图
+- (void) wl_reload;
+//设置总体内容的左右边距,主要针对左右，默认是0
+- (void) wl_setMargin:(CGFloat)margin;
+//设置左或者右每两个item之间的间距,默认是0
+- (void) wl_setItemSpace:(CGFloat)space;
+@end
+
+NS_ASSUME_NONNULL_END
